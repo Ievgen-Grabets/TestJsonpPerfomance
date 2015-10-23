bc434685949c4fcaa77adadc5dd3d1bfc705c246
Ievgen Grabets <i.grabets@aimprosoft.com>
2015-10-23 14:47:01 +0300

    add statistic on tests

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { server_url: 'http://52.23.184.31:8080/' });
+  res.render('index', { server_url: 'http://52.23.184.31:8080/', number_of_tests_server: 200 });
 });
 
 module.exports = router;
